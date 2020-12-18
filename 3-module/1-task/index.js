4948a4a5c563b4152af22c89954fbdb9c455f8a4
Иванов и Ко <32426965+kinoafisharu@users.noreply.github.com>
2020-12-18 21:06:20 +0300

    1 задача 3 модуля

--- 3-module/1-task/index.js
+++ 3-module/1-task/index.js
@@ -1,7 +1,20 @@
-/**
- * @param   {{ name: string, age: number }[]} users
- * @returns {string[]}  объект
- */
+// пример использования метода map
+// (преобразование объекта, применив к нему
+//   функцию)
+// 
+// Из массива объектов персон сформирован
+// массив имен этих персон
+
+let vasya = { name: 'Вася', age: 25 }
+let petya = { name: 'Петя', age: 30 }
+let masha = { name: 'Маша', age: 28 }
+
+let users = [ vasya, petya, masha ]
+
 function namify(users) {
-  return users.map((user) => user.name);
+  return users.map((user) => user.name)
 }
+
+let names = namify(users)
+
+console.log(names)
